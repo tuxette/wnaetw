ed241afe6f188a0f2fd4e7f00301d8ee2269a9eb
Nathalie Villa-Vialaneix <nathalie.villa@univ-paris1.fr>
2013-08-17 11:16:41 +0200

    corrected several bugs: the application is working now

--- scripts/scripts.R
+++ scripts/scripts.R
@@ -1,40 +1,34 @@
-WhatMyTeacherWants <-
-function(x){
+WhatMyTeacherWants <- function(x) {
 	res <- c(
-		round(mean(x, na.rm=TRUE), digits=2),
-		round(median(x, na.rm=TRUE), digits=2),
-		round(min(x, na.rm=TRUE), digits=2),
-		max(x, na.rm=TRUE),
+		round(mean(x, na.rm=TRUE), digits=2), 
+    round(median(x, na.rm=TRUE), digits=2),
+		round(min(x, na.rm=TRUE), digits=2), max(x, na.rm=TRUE),
 		max(x, na.rm=TRUE)-min(x, na.rm=TRUE),
-		round(sd(x, na.rm=TRUE)),
-		round(kurtosis(x, type=1,na.rm=TRUE), digits=2),
+		round(sd(x, na.rm=TRUE)),	round(kurtosis(x, type=1,na.rm=TRUE), digits=2),
 		round(skewness(x, type=1, na.rm=TRUE), digits=2),
 		round(sd(x, na.rm=TRUE)/mean(x, na.rm=TRUE), digits=2),
 		round(quantile(x,probs=c(0.25,0.75), na.rm=TRUE), digits=2),
 		round(ineq(x[!is.na(x)]),2))
-	names(res) <- c("mean","median","min","max","range","sd","kurtosis","skewness","variation","Q1","Q3","gini")
+	names(res) <- c("mean","median","min","max","range","sd","kurtosis",
+                  "skewness","variation","Q1","Q3","gini")
 	res
 }
 
 apply.wmtw <- function(df) {
-  res <- NULL
-  error <- NULL
   # Select numerical variables
   numVar <- sapply(1:ncol(df),function(x){is.numeric(df[,x])})
 	if (sum(numVar)==0) {
-		error <- "No numerical variables in the data: nothing to do...!"
-	} else {
-		# Run 'WhatMyTeacherWants' on numerical variables
-		res <- apply(df[,numVar],2,WhatMyTeacherWants)
-	}
-  list("res"=res,"error"=error)
+    return(NULL)
+		stop("No numerical variables in the data: nothing to do...!")
+	} else res <- apply(df[,numVar],2,WhatMyTeacherWants)
+
+  res
 }
 
-make.boxplot <- function(df,main="Scaled variables boxplots",xlab="Variables",ylab="",scale=TRUE) {
+make.boxplot <- function(df,main,xlab,ylab,scale,col) {
 	# Select numerical variables
   numVar <- sapply(1:ncol(df),function(x){is.numeric(df[,x])})
-  if (scale) {
-		df[,numVar] <- scale(df[,numVar])
-	}
-  boxplot(as.data.frame(df[,numVar]),main=main,xlab=xlab,ylab=ylab,las=3,cex=0.7,col="royalblue1")
+  if (scale) df[,numVar] <- scale(df[,numVar])
+  boxplot(as.data.frame(df[,numVar]),main=main,xlab=xlab,ylab=ylab,las=3,
+          cex=0.7,col=col)
 }
--- server.R
+++ server.R
@@ -1,52 +1,46 @@
 source("scripts/scripts.R")
-library(ineq)
-library(e1071)
-library(graphics)
-library(stats)
-## Change this
+
 shinyServer(function(input, output) {
-	# This function loads the dataset using the different options specified by the user in ui.R
-	datasetInput <- reactive(function() {
-	  in.file <- input$dfile
-	  
-	  if (is.null(in.file))
-	    return(NULL)
-	  
-	  if (input$rownames) {
-	    read.table(in.file$datapath, header=input$header, sep=input$sep,
-	               quote=input$quote, row.names=1, dec=input$dec)
-	  } else {
-	    read.table(in.file$datapath, header=input$header, sep=input$sep,
-	               quote=input$quote, dec=input$dec)
-	  }
+  dInput <- reactive({
+    in.file <- input$file1
+    
+    if (is.null(in.file))
+      return(NULL)
+    
+    if (input$rownames) {
+      read.table(in.file$datapath, header=input$header, sep=input$sep,
+               quote=input$quote, row.names=1, dec=input$dec)
+    } else {
+      read.table(in.file$datapath, header=input$header, sep=input$sep,
+                 quote=input$quote, dec=input$dec)
+    }
+  })
+  
+  output$view <- renderTable({
+    d.input <- dInput()
+    if (is.null(d.input)) return(NULL)
+    if (ncol(d.input>10)) d.input <- d.input[,1:10]
+    head(dInput(), n=50)  
+  })
+  
+  # This function calculates the output sent to the main panel in ui.R
+  output$summary <- renderTable({
+    d.input <- dInput()
+    t(apply.wmtw(d.input))
+  })
+  
+  output$downloadSummary <- downloadHandler(
+    filename = "summary.csv",
+    content = function(file) {
+      write.csv(apply.wmtw(dInput()), file)
   })
 
-	output$view <- renderTable({
-		all.data <- datasetInput()
-		if (nrow(all.data)>50) {
-			all.data <- all.data[1:50,]
-		}
-		if (ncol(all.data)>10) {
-			all.data <- all.data[,1:10]
-		}
-		all.data
-  })
-
-	# This function calculates the output sent to the main panel in ui.R
-	output$summary <- renderTable({
-		dataset <- datasetInput()
-		t(apply.wmtw(dataset)$res)
-	})
-
-	output$downloadSummary <- downloadHandler(
-		filename = "summary.csv",
-		content = function(file) {
-			write.csv(apply.wmtw(datasetInput())$res, file)
-	})
-	
   # This function makes a boxplot for the numeric variables in the data set
-	output$boxplots <- reactivePlot(function() {
-		make.boxplot(datasetInput(),main=input$main,xlab=input$xlab,ylab=input$ylab,
+  output$boxplots <- renderPlot({
+    make.boxplot(dInput(),main=input$main,xlab=input$xlab,ylab=input$ylab,
                  scale=input$scale, col=input$color)
-	})
+  })
 })
+
+
+
